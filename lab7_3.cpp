4f78ed43f186a0e817ee70a4edf6cc892bbb9c3f
H <f@gmail.com>
2023-12-08 23:11:19 +0700

    lab7_3Complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,13 @@
-#include<iostream>
-using namespace std;
+//ไม่ต้องเติม function main()
 
 char before(char x){
-	//Write your function definition here
+if(x == 65 ){
+return 'Z';
 }
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    if( x >= 'B' && x <= 'Z'){
+     x-- ;
+    }
+    else{
+return '0';}
+ return x;
 }
